36517dfe7392fdb93a9ae9e9bcf8269277b04c82
Saman Sathenjeri <saman.m.sath@gmail.com>
2025-10-02 16:40:10 -0400

    updating CI/CD for musl build
    Signed-off-by: Saman Sathenjeri <saman.m.sath@gmail.com>

--- spnl/src/lib.rs
+++ spnl/src/lib.rs
@@ -1,6 +1,24 @@
 mod query;
 pub use query::*;
 
+#[cfg(feature = "openssl-vendored")]
+use openssl_probe;
+
+use pyo3::prelude::*;
+
+#[cfg(feature = "openssl-vendored")]
+fn probe_ssl_certs() {
+    openssl_probe::init_ssl_cert_env_vars();
+}
+
+#[cfg(not(feature = "openssl-vendored"))]
+fn probe_ssl_certs() {}
+
+#[pyfunction]
+pub fn main() {
+    probe_ssl_certs();
+}
+
 #[cfg(feature = "run")]
 mod execute;
 #[cfg(feature = "run")]
